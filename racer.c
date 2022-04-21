777a47918f1eb0b288a3f3748ded8859e24da86c
Ming E Creekmore <mec5765@cs.rit.edu>
2022-04-21 02:46:06 -0400

    Implemented make_racer and destroy_racer functions in racer.c

--- racer.c
+++ racer.c
@@ -8,16 +8,29 @@
 //
 // // // // // // // // // // // // // // // // // // // //
 
+#include "racer.h"
+
 void init_racers(long milliseconds, int length) {
 
 }
 
+//makes a new racer given the name of the racer and the row num
 Racer * make_racer(char *name, int row) {
-
+    Racer * new_racer = (Racer *) malloc(sizeof(Racer));
+    new_racer->row = row;
+    new_racer->distance = 0;
+    new_racer->graphic = (char *) malloc(MAX_CAR_LEN + 1);
+    new_racer->graphic = "~0=-------o>";
+    for(int i = 0; i < strlen(name); i++) {
+        new_racer->graphic[i+3] = name[i];
+    }
+    return new_racer;
 }
 
+//destroys a racer instance
 void destroy_racer(Racer *racer) {
-
+    free(racer->graphic);
+    free(racer);
 }
 
 void* run(void *racer) {
